a7965e1d1519e9c21701f55db08c9d457ffb3a56
Charley Shi <charleyshi02@gmail.com>
2022-04-14 20:10:01 +1200

    Schematic and LED footprint done.

---
+++ Code/RGB_Lamp/main.c
@@ -0,0 +1,18 @@
+/*
+ * RGB_Lamp.c
+ *
+ * Created: 4/14/2022 6:41:08 PM
+ * Author : Charley
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
